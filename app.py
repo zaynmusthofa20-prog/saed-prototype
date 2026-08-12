57fd511befa9856657df7ff2fbbfcdd4ee79351b
zaynmusthofa20-prog <zaynmusthofa20@gmail.com>
2026-08-13 04:31:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import re
 import streamlit as st
 import plotly.graph_objects as go
@@ -362,7 +363,5 @@ a,b=st.columns([1,2])
 with a:
     fig=go.Figure(go.Pie(values=[overall,100-overall], hole=.76, textinfo="none",
                          marker=dict(colors=["#20cfff","#1a2a58"])))
-    File "/mount/src/saed-prototype/app.py", line 365
-      fig.update_layout(height=230), margin=dict(l=0,r=0,t=0,b=0), showlegend="False",
-                                     ^
-SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+    fig.update_layout(height=230, margin=dict(l=0,r=0,t=0,b=0), showlegend=False,
+                      paper_bgcolor="rg
