a71db3bd0f85c2a2ad2da070b806a14170ee69ec
zaynmusthofa20-prog <zaynmusthofa20@gmail.com>
2026-08-13 01:52:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
     page_title="SAED - Social Achievement Exposure Detector",
     page_icon="­ЪДа",
     layout="centered"
-)
+
 
 # =========================================================
 # STYLE
